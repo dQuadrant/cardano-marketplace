f1fbc74b9839399000cd47348dfd1ddd4841755b
nabin6246 <nabin62246@gmail.com>
2022-06-10 10:35:41 +0545

    Add simple market place contract code

---
+++ marketplace-plutus/Plutus/Contracts/V1/SimpleMarketplace.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE NoImplicitPrelude  #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE NumericUnderscores#-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+module Plutus.Contracts.V1.SimpleMarketplace(
+  simpleMarketplacePlutus,
+  simpleMarketValidator,
+  simpleMarketScript,
+  MarketRedeemer(..),
+  SimpleSale(..)
+)
+where
+
+import GHC.Generics (Generic)
+import PlutusTx.Prelude
+import Prelude(Show)
+import qualified Prelude
+import  PlutusTx hiding( txOutDatum)
+import Data.Aeson (FromJSON, ToJSON)
+import qualified PlutusTx.AssocMap as AssocMap
+import qualified Data.Bifunctor
+import Plutus.V1.Ledger.Api
+import Plutus.V1.Ledger.Value ( assetClassValue, geq, AssetClass(..),CurrencySymbol(..),TokenName(..), assetClassValueOf )
+import Plutus.V1.Ledger.Contexts (valuePaidTo, ownHash, valueLockedBy, findOwnInput, findDatum,txSignedBy)
+import Plutus.V1.Ledger.Address (toPubKeyHash, scriptHashAddress, toValidatorHash)
+import Plutus.V1.Ledger.Scripts (getScriptHash, ScriptHash (ScriptHash))
+import qualified Data.ByteString.Short as SBS
+import qualified Data.ByteString.Lazy  as LBS
+import Cardano.Api.Shelley (PlutusScript (..), PlutusScriptV1)
+import Codec.Serialise ( serialise )
+
+
+{-# INLINABLE allScriptInputsCount #-}
+allScriptInputsCount:: ScriptContext ->Integer
+allScriptInputsCount ctx@(ScriptContext info purpose)=
+    foldl (\c txOutTx-> c + countTxOut txOutTx) 0 (txInfoInputs  info)
+  where
+  countTxOut (TxInInfo _ (TxOut addr _ _)) = if isJust (toValidatorHash addr) then 1 else 0
+
+
+data MarketRedeemer =  Buy | Withdraw
+    deriving (Generic,FromJSON,ToJSON,Show,Prelude.Eq)
+PlutusTx.makeIsDataIndexed ''MarketRedeemer [('Buy, 0), ('Withdraw, 1)]
+
+
+data SimpleSale=SimpleSale{
+    sellerAddress:: Address, -- The main seller Note that we are using address 
+    priceOfAsset:: Integer  -- cost of the value in it
+  } deriving(Show,Generic)
+
+
+PlutusTx.makeIsDataIndexed ''SimpleSale [('SimpleSale, 0)]    
+
+{-# INLINABLE mkMarket #-}
+mkMarket ::  SimpleSale   -> MarketRedeemer -> ScriptContext    -> Bool
+mkMarket  ds@SimpleSale{sellerAddress,priceOfAsset}  action ctx =
+  case sellerPkh of 
+    Nothing -> traceError "Script Address in seller"
+    Just pkh -> case  action of
+        Buy       -> traceIfFalse "Multiple script inputs" (allScriptInputsCount  ctx == 1)  && 
+                     traceIfFalse "Seller not paid" (assetClassValueOf   (valuePaidTo info pkh) adaAsset >= priceOfAsset)
+        -- Withdraw  -> traceIfFalse "Withdraw: Missing signatåure" ( txSignedBy  info pkh )
+    where
+      sellerPkh= case sellerAddress of { Address cre m_sc -> case cre of
+                                                           PubKeyCredential pkh -> Just pkh
+                                                           ScriptCredential vh -> Nothing  }
+      info  =  scriptContextTxInfo ctx
+      adaAsset=AssetClass (adaSymbol,adaToken )
+
+{-# INLINABLE mkWrappedMarket #-}
+mkWrappedMarket ::  BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkWrappedMarket  d r c = check $ mkMarket (unsafeFromBuiltinData d) (unsafeFromBuiltinData r) (unsafeFromBuiltinData c)
+
+
+simpleMarketValidator ::   Validator
+simpleMarketValidator = mkValidatorScript  $
+            $$(PlutusTx.compile [|| mkWrappedMarket ||])
+
+simpleMarketScript ::   Script
+simpleMarketScript  =  unValidatorScript  simpleMarketValidator
+
+marketScriptSBS :: SBS.ShortByteString
+marketScriptSBS  =  SBS.toShort . LBS.toStrict $ serialise $ simpleMarketScript 
+
+simpleMarketplacePlutus ::  PlutusScript PlutusScriptV1
+simpleMarketplacePlutus  = PlutusScriptSerialised $ marketScriptSBS
