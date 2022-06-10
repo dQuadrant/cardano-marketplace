d1071b2e34fda2b610aa584be7370ed173711eb8
nabin6246 <nabin62246@gmail.com>
2022-06-10 11:46:40 +0545

    Remove withdraw from op

--- marketplace-cli/Cli.hs
+++ marketplace-cli/Cli.hs
@@ -76,11 +76,6 @@ runCli = do
             }
             &= details ["Buy an asset on sale after finiding out txIn from market-cli ls.", "  Eg. buy '8932e54402bd3658a6d529da707ab367982ae4cde1742166769e4f94#0' '{\"fields\":...}'"],
           Ls &= help "List utxos for market",
-          Withdraw
-            { txin = "" &= typ "TxIn" &= argPos 0,
-              datum = "" &= typ "Datum" &= argPos 1
-            }
-            &= details ["Withdraw an asset on sale after finiding out txIn from market-cli ls.", "  Eg. withdraw '8932e54402bd3658a6d529da707ab367982ae4cde1742166769e4f94#0' '{\"fields\":...}'"],
           Mint &= help "Mint a new asset",
           CreateCollateral &= help "Create a new collateral utxo."
         ]
