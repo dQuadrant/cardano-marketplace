f84aa7be045c314d026ae1137a5da8acb8a63381
nabin6246 <nabin62246@gmail.com>
2022-06-14 15:38:32 +0545

    Add default operator pub key hash in cli

--- marketplace-cli/Cli.hs
+++ marketplace-cli/Cli.hs
@@ -133,8 +133,9 @@ runCli = do
         &= summary "Cardano Marketplace CLI \nVersion 1.0.0.0"
 
   chainInfo <- chainInfoFromEnv
+  opConfig <- getDefaultOperatorConfig
   let marketAddr = marketAddressShelley (getNetworkId chainInfo)
-  let offerAddr = offerAddressShelley (getNetworkId chainInfo)
+  let offerAddr = offerAddressShelley (getNetworkId chainInfo) opConfig
   case op of
     Ls -> do
       utxos <- queryMarketUtxos chainInfo marketAddr
@@ -147,7 +148,7 @@ runCli = do
           nullUtxo (UTxO mp) = Map.null mp
     Cat -> do
       let marketScriptCbor = serialiseToCBOR simpleMarketplacePlutus
-      let offerScriptCbor = serialiseToCBOR offerScriptPlutus
+      let offerScriptCbor = serialiseToCBOR $ offerScriptPlutus opConfig
       putStrLn $  "Marketplace :\n" ++ toHexString marketScriptCbor
       putStrLn $  "OfferScript :\n" ++ toHexString offerScriptCbor
     Sell itemStr cost sKeyFile-> do
@@ -156,7 +157,7 @@ runCli = do
     Offer asset amount skeyFile -> do 
       skey <- getSignKey skeyFile
       asset <- parseAssetIdText asset
-      offerToken chainInfo asset amount skey
+      offerToken chainInfo asset amount skey opConfig
     Buy txInText datumStr sKeyFile-> do
       sKey <- getSignKey sKeyFile
       buyToken chainInfo txInText datumStr sKey marketAddr
@@ -186,3 +187,15 @@ getSignKey skeyfile =
                             pure  $ home ++  drop 1 skeyfile
                             )
                           else pure skeyfile
+
+
+getDefaultOperatorConfig :: IO OperatorConfig
+getDefaultOperatorConfig = do
+  opPkh <-case deserialiseAddress AsAddressAny "addr_test1vq66k7sxqef27nwkr4htffs6lgr4pyg5aut4s7ugfclqt5qgxpnz9" of
+      Nothing -> throwIO $ userError "Error : Invalid address format used for operator."
+      Just (AddressShelley shelleyAddr) -> return $ unMaybe "Error operator address as script address is not supported." $ shelleyPayAddrToPlutusPubKHash shelleyAddr
+      Just _ -> throwIO $ userError "Error : Byron address is not supported."
+  pure OperatorConfig {
+      operatorPkh = opPkh,
+      operatorFee = 1 -- Operator fee is 1 ada
+    }
